c1a405eda3f4d10f233780eabbde4f2ad8f8c7f6
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-12-15 12:39:08 +0100

    feat(task 1.1): implement capitalizeEachWord

--- algorithms-junior-training/1-strings/capitalizeEachWord.js
+++ algorithms-junior-training/1-strings/capitalizeEachWord.js
@@ -4,7 +4,15 @@
  */
 
 function capitalizeEachWord(str) {
-  // Your code here
+  let words = str.split(' ');
+
+  let capelizedFirstLetterWord = words.map(
+    (word) => word[0].toUpperCase() + word.slice(1),
+  );
+  let result = capelizedFirstLetterWord.join(' ');
+  console.log(result);
+  return result;
 }
+capitalizeEachWord('hello world');
 
 module.exports = capitalizeEachWord;
