5d8177fa9d54743ea27e7d517e9a5dde14ff2626
Yuliia Ozkan <yulka.oak18@gmail.com>
2025-12-16 23:47:43 +0100

    feat(task 1.3): implement compressString

--- algorithms-junior-training/1-strings/3-compressString.js
+++ algorithms-junior-training/1-strings/3-compressString.js
@@ -5,9 +5,9 @@
 
 function compressString(str) {
   let result = '';
-  let count = 0;
+  let count = 1;
 
-  for (let i = 0; i <= str.length; i++) {
+  for (let i = 1; i <= str.length; i++) {
     if (str[i] === str[i - 1]) {
       count++;
     } else {
